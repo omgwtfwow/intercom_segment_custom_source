9ce579dc982400845b1a3b3ce98ed0ce7be312e8
Juan G <hello@juangonzalez.com.au>
2019-08-17 10:02:34 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Integrates Intercom's webhooks with Segment's custom sources
+Docs:<br>
+Segment https://segment.com/docs/sources/custom/<br>
+Live chat events spec https://segment.com/docs/spec/live-chat/ <br>
+Intercom https://developers.intercom.com/building-apps/docs/webhooks <br>
---
+++ handler.js
@@ -0,0 +1,71 @@
+var get = require('lodash.get')
+
+exports.processEvents = async (event) => {
+    let eventBody = event.payload.body;
+    let eventHeaders = event.payload.headers;
+    let queryParameters = event.payload.queryParameters;
+
+    let intercomEvent = eventBody['topic'];
+    let events = {
+        "conversation.user.created": "Live Chat Conversation Started",
+        "conversation.admin.closed": "Live Chat Conversation Ended",
+        "conversation.user.replied": "Live Chat Message Sent",
+        "conversation.admin.replied": "Live Chat Message Received",
+        "conversation.admin.single.created": "Live Chat Message Received"
+    };
+    let eventName = function () {
+        if (events[intercomEvent] !== 'null' || events[intercomEvent] !== null || events[intercomEvent] !== null) {
+            return events[intercomEvent];
+        } else {
+            return false;
+        }
+    }
+    if (eventName === false) {
+        return false;
+    }
+    let item = get(eventBody, 'data["item"]', false);
+    if (item === false) {
+        return false;
+    }
+    let user = get(item, 'user', false);
+    if (user === false) {
+        return false;
+    }
+    let userId = user.user_id;
+    if (userId === undefined || userId === null || userId === "null" || userId === "") {
+        return false;
+    }
+    let userTraits = {
+        userId: userId,
+        email: user.email,
+        name: user.name,
+        type: user.type
+    };
+    let assignee = get(item, 'assignee', false);
+    if (assignee === false) {
+        return false;
+    }
+    let eventProperties = {
+        agent_id: assignee.id,
+        agent_username: assignee.name,
+        agent_email: assignee.email,
+        conversation_id: item.id,
+        message_id: eventBody.id,
+        conversation_url: item.links.conversation_web
+    };
+    let returnValue = {
+        events: [{
+            type: 'identify',
+            userId: userId,
+            traits: userTraits
+        },
+            {
+                type: 'track',
+                event: eventName(),
+                userId: userId,
+                properties: eventProperties
+            }]
+    };
+
+    return (returnValue)
+};
